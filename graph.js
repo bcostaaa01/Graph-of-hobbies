ef52470a15cbece7ac7e56d30515342baba07737
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2022-07-06 21:24:45 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,3 +1,5 @@
+import * as d3 from "https://cdn.skypack.dev/d3@7";
+
 let friends = [{name: "Bruno", age: 20}, {name: "Carl", age: 25}, {name: "John", age: 23}, {name: "Tim", age: 28}, {name: "Marco", age: 30}, {name: "Mary", age: 22}, {name: "Melanie", age: 20}]
 
 let divSelection = d3.select("body")
