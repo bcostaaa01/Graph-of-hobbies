164f803c1337503f4d2a75b5292da7fc042363f8
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2022-07-06 16:43:32 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,5 +1,11 @@
 let friends = [{name: "Bruno", age: 20}, {name: "Carl", age: 25}, {name: "John", age: 23}, {name: "Tim", age: 28}, {name: "Marco", age: 30}, {name: "Mary", age: 22}, {name: "Melanie", age: 20}]
 
-let divSelection = d3.select("body").selectAll("div")
+let divSelection = d3.select("body")
+.selectAll("div")
 
-divSelection.data(friends).enter().append("div").text(function (d) {return d.name + "is " + d.age + " years old"}).attr("class", "bar").style("width", function (d) { return 60 * d.age + "px"})
+divSelection.data(friends)
+  .enter()
+  .append("div")
+  .text(function (d) {return d.name + "is " + d.age + " years old"})
+  .attr("class", "bar")
+  .style("width", function (d) { return 60 * d.age + "px"})
